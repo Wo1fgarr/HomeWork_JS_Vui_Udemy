ff47b0933e351afb27db3bbbc667ba364d71f599
Wo1fgarr <cotedor13@gmail.com>
2021-10-17 14:22:45 +0300

    switch case Homework

--- 1_Basic_JS/app.js
+++ 1_Basic_JS/app.js
@@ -194,3 +194,30 @@
 // }
 
 //! Домашнее задание - Тернарные операторы и switch case
+// 1
+let a = 'other';
+
+switch(a){
+  case 'block':
+    console.log('block')
+    break;
+  case 'none':
+    console.log('none')
+    break;
+  case 'inline':
+    console.log('inline')
+    break;
+  default:
+    console.log('other')
+}
+
+// 2
+let b = 'hidden';
+b === 'hidden' ? b = 'visible': b = 'hidden';
+
+// 3
+let c = 0;
+
+c === 0 ? c = 1 : 
+c < 0 ? c = 'less then zero' : 
+c *= 10;
